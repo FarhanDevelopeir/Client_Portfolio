5988901a533cb5ebaab4d8366a53c43479b44bbe
Muhammad farhan <fb4459467@gmail.com>
2024-09-24 22:50:10 -0700

    admin page heading

--- frontend/src/components/Admin_FIles/Admin_component.jsx
+++ frontend/src/components/Admin_FIles/Admin_component.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 const Admin_component = () => {
-  return (
-    <div className=' h-screen'>Admin_component</div>
-  )
-}
+  return <div className=" h-screen pt-32 font-semibold text-5xl text-center">
+      Admin Component
+      <p className="text-gray-600 text-sm mt-6">Admin page is in underworking it will be updated soon</p>
+  </div>;
+};
 
-export default Admin_component
+export default Admin_component;

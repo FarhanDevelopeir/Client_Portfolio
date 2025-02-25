bc1ec152ac57786d7fdb1c41124e413317c1a446
FarhanDevelopeir <fb4459467@gmail.com>
2025-02-25 13:14:21 +0500

    new changes

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import bg_img from "../assets/images/bgimg.png"
 import { useDispatch, useSelector } from 'react-redux';
 import axios from 'axios';
